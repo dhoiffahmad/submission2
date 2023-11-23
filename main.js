300898fb165cc4ee7dc8b4cd9da77237324e364b
dhoiffahmad <dhoiff.ahmad@gmail.com>
2023-11-23 17:07:26 +0700

    sekarang hapus yg dikerjakan

--- main.js
+++ main.js
@@ -1,112 +1,136 @@
 // TAHAP 1 MENGIRIM DATA KE CONSOLE
 
-const books = [];
-const RENDER_EVENT = 'render-book';
+const beberapaBuku = [
+  {
+    id: 1,
+    judul: "Satu Dasar",
+    penulis: "Dhoifur",
+    penerbit: "Gramedia",
+    tahun: "2020",
+    isComplete: true,
+  },
+  {
+    id: 3,
+    judul: "Petualang",
+    penulis: "Malik Abdurrohman",
+    penerbit: "Gramedia",
+    tahun: "2020",
+    isComplete: false,
+  },
+];
 
-document.addEventListener('DOMContentLoaded', function () {
-    const inputBookForm = document.getElementById('inputBook');
-    inputBookForm.addEventListener('submit', function (event) {
-      event.preventDefault();
-      addBook();
-    });
+const RENDER_EVENT = "render";
+const formInput = document.getElementById("inputBook");
+console.log(formInput);
+const inputJudul = document.getElementById("inputJudul");
+const inputPenulis = document.getElementById("inputPenulis");
+const inputPenerbit = document.getElementById("inputPenerbit");
+const inputTahun = document.getElementById("inputTahun");
+const inputCheckbox = document.getElementById("inputCheckbox");
+const content = document.getElementById("content");
+
+// Membuat custome event
+document.addEventListener(RENDER_EVENT, function () {
+  content.innerHTML = "";
+  const booksComplete = beberapaBuku.filter((book) => {
+    return book.isComplete == true;
   });
 
-
-  function addBook() {
-    // Mengambil nilai dari elemen formulir
-    const title = document.getElementById("inputBookTitle").value;
-    const author = document.getElementById("inputBookAuthor").value;
-    const year = document.getElementById("inputBookYear").value;
-    const isComplete = document.getElementById("inputBookIsComplete").checked;
-
-    // Menyimpan semua data dalam objek array
-    // const generatedID = generateId();
-    // const book = generateTodoObject(generatedID, title, author, year, isComplete);
-    
-     // Membuat objek buku baru
-    const book = {
-      id: generateId(), // Memanggil fungsi untuk menghasilkan ID unik
-      title: title,
-      author: author,
-      year: year, 
-      isComplete: isComplete,
-    };
-
-    
-    books.push(book);
-   
-    document.dispatchEvent(new Event(RENDER_EVENT));
-  }
-
-  function generateId() {
-    return +new Date();
-  }
-
-//   Mengirim data dengan cunstome event 'RENDER_EVENT'
-  document.addEventListener(RENDER_EVENT, function () {
-    console.log(books);
-
- // Mendapatkan elemen daftar buku belum selesai dan selesai
-  const incompleteBookshelf = document.getElementById("incompleteBookshelfList");
-  const completeBookshelf = document.getElementById("completeBookshelfList");
-
-  // Mengosongkan elemen daftar buku
-  incompleteBookshelf.innerHTML = "";
-  completeBookshelf.innerHTML = "";
-    
-    for (const bookItem of books) {
-        const bookElement = makeTodo(bookItem);
-        if (bookItem.isComplete) {
-          completeBookshelf.appendChild(bookElement);
-        } else {
-          incompleteBookshelf.appendChild(bookElement);
-        }
-    }
-     });
-
-
-//  TAHAP 2 MEMINDAH DATA KE HTML
-
-// Fungsi untuk membuat elemen HTML dari objek buku
-function makeTodo(book) {
-  const bookItem = document.createElement("article");
-  bookItem.classList.add("book_item");
-
-  const title = document.createElement("h3");
-  title.innerText = book.title;
-
-  const author = document.createElement("p");
-  author.innerText = `Penulis: ${book.author}`;
-
-  const year = document.createElement("p");
-  year.innerText = `Tahun: ${book.year}`;
-
-  const actionDiv = document.createElement("div");
-  actionDiv.classList.add("action");
-
-  const actionButton = document.createElement("button");
-  actionButton.innerText = book.isComplete
-    ? "Belum selesai dibaca"
-    : "Selesai dibaca";
-  actionButton.classList.add(book.isComplete ? "green" : "red");
-  actionButton.addEventListener("click", function () {
-    toggleBookStatus(book.id); // Memanggil fungsi untuk mengubah status buku
+  const booksUncomplete = beberapaBuku.filter((book) => {
+    return book.isComplete == false;
   });
 
-  const deleteButton = document.createElement("button");
-  deleteButton.innerText = "Hapus buku";
-  deleteButton.classList.add("red");
-  deleteButton.addEventListener("click", function () {
-    deleteBook(book.id); // Memanggil fungsi untuk menghapus buku
+  // Buat Content 1 / Selesai dibaca
+  const content1 = document.createElement("div");
+  content1.setAttribute("id", "content1");
+
+  const content1H2 = document.createElement("h2");
+  content1H2.innerText = "Selesai dibaca";
+  content1.append(content1H2);
+
+  // Isi Content 1
+  booksComplete.forEach((book) => {
+    const p1 = document.createElement("p");
+    const p2 = document.createElement("p");
+    const p3 = document.createElement("p");
+    const p4 = document.createElement("p");
+    const p5 = document.createElement("p");
+
+    const tombolDiv = document.createElement("div");
+    const ubah = document.createElement("button");
+    ubah.setAttribute("id", "ubah1");
+    ubah.innerText = "Belum dibaca";
+    const hapus = document.createElement("button");
+    hapus.setAttribute("id", "hapus1");
+    hapus.innerText = "Hapus";
+    tombolDiv.append(ubah, hapus);
+    console.log(tombolDiv);
+
+    p1.innerText = `Judul : ${book.id}`;
+    p2.innerText = `Judul : ${book.judul}`;
+    p3.innerText = `Penulis : ${book.penulis}`;
+    p4.innerText = `Penerbit : ${book.penerbit}`;
+    p5.innerText = `Tahun : ${book.tahun}`;
+    content1.append(p1, p2, p3, p4, p5, tombolDiv);
   });
 
-  actionDiv.appendChild(actionButton);
-  actionDiv.appendChild(deleteButton);
+  // Buat Content 2 / Belum selesai dibaca
+  const content2 = document.createElement("div");
+  content2.setAttribute("id", "content2");
 
-  bookItem.appendChild(title);
-  bookItem.appendChild(author);
-  bookItem.appendChild(year);
-  bookItem.appendChild(actionDiv);
+  const content2H2 = document.createElement("h2");
+  content2H2.innerText = "Belum selesai dibaca";
+  content2.append(content2H2);
 
-  return bookItem;
-  } 
+  // Isi Content 2
+  booksUncomplete.forEach((book) => {
+    const p1 = document.createElement("p");
+    const p2 = document.createElement("p");
+    const p3 = document.createElement("p");
+    const p4 = document.createElement("p");
+    const p5 = document.createElement("p");
+
+    const tombolDiv = document.createElement("div");
+    const ubah = document.createElement("button");
+    ubah.setAttribute("id", "ubah2");
+    ubah.innerText = "Selesai dibaca";
+    const hapus = document.createElement("button");
+    hapus.setAttribute("id", "hapus2");
+    hapus.innerText = "Hapus";
+    tombolDiv.append(ubah, hapus);
+    console.log(tombolDiv);
+
+    p1.innerText = `Judul : ${book.id}`;
+    p2.innerText = `Judul : ${book.judul}`;
+    p3.innerText = `Penulis : ${book.penulis}`;
+    p4.innerText = `Penerbit : ${book.penerbit}`;
+    p5.innerText = `Tahun : ${book.tahun}`;
+    content2.append(p1, p2, p3, p4, p5, tombolDiv);
+  });
+
+  // Masukkan content1 & content2 ke div content
+  content.append(content1, content2);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.dispatchEvent(new Event(RENDER_EVENT));
+});
+
+formInput.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const book = {
+    id: +new Date(),
+    judul: inputJudul.value,
+    penulis: inputPenulis.value,
+    penerbit: inputPenerbit.value,
+    tahun: inputTahun.value,
+    isComplete: inputCheckbox.checked,
+  };
+
+  beberapaBuku.push(book);
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
+
+  formInput.reset();
+});
